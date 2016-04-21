675e555d93c64c5638752e6693d1f713a1fc97b7
liuzhuang <Og192>
2016-04-21 16:45:57 +0800

    delete fiels

---
+++ dataStructure/list/list.h
@@ -0,0 +1,18 @@
+#include<list>
+struct ListNode
+{
+    int Data;
+    ListNode *Next;
+}
+
+template<class item_type>
+class List
+{
+    public:
+        List(item_type)
+        List *Search(item_type x);
+        List *Insert();
+        List *Precursor();
+    private:
+        ListNode *mHead
+};
