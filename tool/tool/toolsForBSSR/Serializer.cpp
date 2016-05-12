eee2f997b7b2fca87258e50816db9333df5aa1bc
Og192 <og192liu@gmai.com>
2016-05-12 17:44:27 +0800

    c++ seri

---
+++ tool/tool/toolsForBSSR/Serializer.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+
+vector<string> *split(string line)
+{
+    istringstream toLine(line);
+    vector<string> *tokens = new vector<string>(istream_iterator<string>{toLine}, istream_iterator<string>{});
+//    copy(
+//        istream_iterator<string>(toLine),
+//        istream_iterator<string(),
+//        back_inserter(tokens)
+//    )
+    return tokens;
+}
+
+vector<string> *split(string line, char delim)
+{
+    istringstream toLine(line);
+    vector<string> *tokens = new vector<string>();
+    string item;
+    while(getline(toLine, item, delim))
+    {
+        tokens->push_back(item);
+    }
+    return tokens;
+}
+
+map<string, string> *generateVectirDict(string vectorDictPath)
+{
+    map<string, string> *vectors = new map<string, string>();
+    
+    int linenumber = 0;
+    
+    ifstream vectorFile;
+    vectorFile.open(vectorDictPath.c_str());
+    if(vectorFile.is_open())
+    {
+        string line = "";
+        while(getline(vectorFile, line))
+        {
+            linenumber++;
+            if(linenumber % 1000 == 0)
+            {
+                cout << "generate vector dict: " << linenumber << endl;
+            }
+            vector<string> *vals = split(line, ' ');
+            string vectorStr = "";
+            for(unsigned int i = 1; i < (*vals).size(); i++)
+            {
+                vectorStr += (*vals)[i];
+            }
+            vectors->insert(pair<string, string>((*vals)[0], vectorStr));
+        }
+            
+    }
+    
+    return vectors;
+}
+
+vector<double> RandInOne(unsigned int size)
+{
+    vector<double> data;
+    for(unsigned int i = 0; i < size; i++)
+    {
+        double a = rand() % 10000 / 10000.0 - 0.5;
+        data.push_back(a);
+    }
+    return data;
+}
+
+template<class T>
+string str(T number)
+{
+    stringstream ss;
+    ss << number;
+    return ss.str();
+}
+
+void generate(string wordlist, map<string, string> &vectors, unsigned int dimension, string dictPath, string serializationPath)
+{
+    map<string, int> dictTable;
+    int dictIndex = 0;
+    ofstream dictFile(dictPath.c_str());
+    ofstream serializationFile(serializationPath.c_str());
+    ifstream wordslistFile(wordlist.c_str());
+
+    /////////////////////////
+    int linenumber = 0;
+    int notFoundCount = 0;
+    /////////////////////////
+    
+    string line;
+    while(getline(wordslistFile, line))
+    {
+        string serialicationNumbersStr = "";
+        linenumber += 1;
+        if(linenumber % 1000 == 0)
+        {
+            cout << serializationPath + " generate serialization: " << linenumber << endl;
+        }
+        vector<string> *words = split(line, ' ');
+        string wordVector = "";
+        for(unsigned int i = 0; i < words->size(); i++)
+        {
+            string word = (*words)[i];
+            if(dictTable.find(word) == dictTable.end())
+            {
+                if(vectors.find(word) != vectors.end())
+                {
+                    wordVector = vectors[word];
+                }
+                else
+                {
+                    notFoundCount += 1;
+                    vector<double> data = RandInOne(dimension);
+                    for(vector<double>::iterator iter = data.begin(); iter != data.end(); iter++)
+                    {
+                        wordVector = str(*iter) + " ";
+                    }
+                }
+                dictIndex += 1;
+                dictTable.insert(pair<string, int>(word, dictIndex));
+                dictFile << wordVector << "\n";
+            }
+            serialicationNumbersStr += str(dictTable[word]) + " ";
+        }
+        serializationFile << serialicationNumbersStr << "\n";
+    }
+    cout << serializationPath << " Done, there are " << notFoundCount << " words which are not found.";
+    dictFile.close();
+    serializationFile.close();
+}
---
+++ tool/tool/toolsForBSSR/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+void maptest()
+{
+    map<string, string> *m = new map<string, string>();
+    m->insert(pair<string, string>("liuzhuang", "12 3 423 4234 234"));
+    m->insert(pair<string, string>("shen", "12 3 423 4234 234"));
+    
+    cout << (*m)["liuzhuang"] << endl;
+    
+    delete m;
+    //m->insert("SDFSD", "SDFSD");
+}
+
+void inFileStreamTest()
+{    
+    const string filepath = "test";
+    ifstream file;
+    file.open(filepath.c_str());
+    string line;
+    if(file.is_open())
+    {
+        while(getline(file, line))
+        {
+            cout << line << '\n';
+        }
+    }
+    file.close();
+}
+void ofilestreamTest()
+{
+    const string filepath = "test";
+    ofstream file;
+    file.open(filepath.c_str());
+    if(!file.is_open())
+    {
+        cout << "cannot open file"+filepath << endl;
+        return;
+    }
+    file << "one line\n";
+    file << "another line\n";
+    file.close();
+}
+
+void stringTest()
+{
+    string vector = "23123 12312 3123 4234 1231 123";
+    //vector.s
+}
+
+vector<string> *splitTest(string line)
+{
+    istringstream toLine(line);
+    vector<string> *tokens = new vector<string>(istream_iterator<string>{toLine}, istream_iterator<string>{});
+//    copy(
+//        istream_iterator<string>(toLine),
+//        istream_iterator<string(),
+//        back_inserter(tokens)
+//    )
+    return tokens;
+}
+
+vector<string> *splitTest(string line, char delim)
+{
+    istringstream toLine(line);
+    vector<string> *tokens = new vector<string>();
+    string item;
+    while(getline(toLine, item, delim))
+    {
+        tokens->push_back(item);
+    }
+    return tokens;
+}
+map<string, string> *generateVectirDict(string vectorDictPath);
+void generate(string wordlist, map<string, string> &vectors, unsigned int dimension, string dictPath, string serializationPath);
+
+int main(int argc, char **argv)
+{
+    string corpusPath = "/home/laboratory/corpus/";
+    string vectorsDict = corpusPath + "en"+"_vectorTable/"+"en"+"_vectors_"+ "50" +".txt";
+    string wordslist = corpusPath + "en" + "/test_"+"book"+"_new.txt.extract";
+    
+
+    string dictPath = "test_book_new.txt.extract_50.lstmDict";
+    string serializationPath = "test_book_new.txt.extract_50.serialization";
+
+    map<string, string> *dict = generateVectirDict(vectorsDict);
+    generate(wordslist, *dict, 50, dictPath, serializationPath);
+    //maptest();
+    //ofilestreamTest();
+    //inFileStreamTest();
+//    vector<string> *strs = NULL;
+//    strs = splitTest("SDF er gda we hyr ", ' ');
+//    for(unsigned int i = 0; i < strs->size(); i++)
+//    {
+//        cout << (*strs)[i] << endl;
+//    }
+//    cout << "(((((((((((((((((((((((((" << endl;
+//    delete strs;
+//    
+//    string str= "sdfsf";
+//    cout << str + str << endl;
+	return 0;
+}
