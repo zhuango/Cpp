35c81122dba46aeeea3cb2a5f3c8230ed657b043
Og192 <og192liu@gmail.com>
2017-01-11 09:29:14 +0800

    unions

---
+++ cppDeepLearn/unions.cpp
@@ -0,0 +1,134 @@
+// A union is a user-defined type in which all members share the same memory location. 
+// This means that at any given time a union can contain no more than one object from its list of members.
+// It also means that no matter how many members a union has,
+// it always uses only enough memory to store the largest member.
+
+// Unions can be useful for conserving memory when you have lots of objects and/or limited memory.
+// However they require extra care to use correctly because you are responsible for ensuring 
+// that you always access the last member that was written to.
+// If any member types have a non-trivial constructor,
+// then you must write additional code to explicitly construct and destroy that member.
+// Before using a union, consider whether the problem you are trying to solve could be better 
+// expressed by using a base class and derived classes.
+#include <iostream>
+#include <queue>
+using namespace std;
+
+namespace BaseUnionTest
+{
+    union RecordType
+    {
+        char    ch;
+        int     i;
+        long    l;
+        float   f;
+        double  d;
+        int     *int_ptr;
+    };
+
+    void testSize()
+    {
+        cout << "union size: " << sizeof(RecordType) << endl;
+    }
+    void testBase()
+    {
+        RecordType t;
+        t.i = 5;    // t holds an int
+        t.f = 7.25; // t now holds a float
+    }
+}
+
+namespace DiscriminatedUnionTest
+{
+    // Of course, the example shows a problem that could also be solved by using classes that derive from
+    // a common base class, and branching your code based on the runtime type of each object in the container.
+    // This may result in code that easier to maintain and understand, but it might also be slower than 
+    // using unions. Also, with a union, you can store completely unrelated types, 
+    // and dynamically change the type of the value that is stored without changing the type of the union 
+    // variable itself. Thus you can create a heterogeneous array of MyUnionType whose elements store different
+    // values of different types. Note that the Input struct in the preceding example can be easily misused.
+    // It is completely up to the user to use the discriminator correctly to access the member that holds 
+    // the data.You can protect against misuse by making the union private and providing special access functions.
+    enum class WeatherDataType
+    {
+        Temperature, Wind
+    };
+
+    struct TempData
+    {
+        int StationId;
+        time_t time;
+        double current;
+        double max;
+        double min;
+    };
+
+    struct WindData
+    {
+        int StationId;
+        time_t time;
+        int speed;
+        short direction;
+    };
+    
+    struct Input
+    {
+        WeatherDataType type;
+        // In the  example, note that the union in the Input struct has no name. 
+        // This is an anonymous union and its members can be accessed 
+        // as if they were direct members of the struct. 
+        union
+        {
+            TempData temp;
+            WindData wind;
+        };
+    };
+
+    void ProcessTemp(TempData t){}
+    void ProcessWind(WindData w){}
+
+    queue<Input> inputs;
+    void Initialize()
+    {
+        Input first, second;
+        first.type = WeatherDataType::Temperature;
+        first.temp = { 101, 1418855664, 91.8, 108.5, 67.2 };
+        inputs.push(first);
+
+
+        second.type = WeatherDataType::Wind;
+        second.wind = { 204,1418859354, 14, 27 };
+        inputs.push(second);
+    }
+    void test()
+    {
+        Initialize();
+        while( !inputs.empty() )
+        {
+            Input i = inputs.front();
+            
+            switch(i.type)
+            {
+                case WeatherDataType::Temperature:
+                    ProcessTemp(i.temp);
+                    break;
+                case WeatherDataType::Wind:
+                    ProcessWind(i.wind);
+                    break;
+                default:
+                    break;
+            }
+            inputs.pop();
+        }
+    }
+}
+
+int main(void)
+{
+
+    BaseUnionTest::testBase();
+    BaseUnionTest::testSize();
+    DiscriminatedUnionTest::test();
+
+    return 0;
+}
---
+++ cppDeepLearn/unrestrictedUnions.cpp
@@ -0,0 +1,455 @@
+// In C++03 and earlier a union can contain non-static data members with class type 
+// as long as the type has no user provided constructors
+// destructors or assignment operators. In C++11, these restrictions are removed.
+// If you include such a member in your union then the compiler will automatically 
+// mark any special member functions that are not user provided as deleted. If the
+// union is an anonymous union inside a class or struct, then any special member 
+// functions of the class or struct that are not user provided are marked as deleted.
+// The following example shows how to handle the case where one of the members of 
+// the union has a member that requires this special treatment
+// Unions cannot store references. Unions don’t support inheritance, therefore a union 
+// itself cannot be used as a base class, or inherit from another class, or have virtual
+// functions.
+
+// for MyVariant  
+#include <crtdbg.h>  
+#include <new>  
+#include <utility>  
+  
+// for sample objects and output  
+#include <string>  
+#include <vector>  
+#include <iostream>  
+  
+using namespace std;  
+  
+struct A   
+{  
+    A() = default;  
+    A(int i, const string& str) : num(i), name(str) {}  
+  
+    int num;  
+    string name;  
+    //...  
+};  
+  
+struct B   
+{  
+    B() = default;  
+    B(int i, const string& str) : num(i), name(str) {}  
+  
+    int num;  
+    string name;  
+    vector<int> vec;  
+    // ...  
+};  
+  
+enum class Kind { None, A, B, Integer };  
+  
+#pragma warning (push)  
+#pragma warning(disable:4624)  
+class MyVariant  
+{  
+public:  
+    MyVariant()  
+        : kind_(Kind::None)  
+    {  
+    }  
+  
+    MyVariant(Kind kind)  
+        : kind_(kind)  
+    {  
+        switch (kind_)  
+        {  
+        case Kind::None:  
+            break;  
+        case Kind::A:  
+            new (&a_) A();  
+            break;  
+        case Kind::B:  
+            new (&b_) B();  
+            break;  
+        case Kind::Integer:  
+            i_ = 0;  
+            break;  
+        default:  
+            _ASSERT(false);  
+            break;  
+        }  
+    }  
+  
+    ~MyVariant()  
+    {  
+        switch (kind_)  
+        {  
+        case Kind::None:  
+            break;  
+        case Kind::A:  
+            a_.~A();  
+            break;  
+        case Kind::B:  
+            b_.~B();  
+            break;  
+        case Kind::Integer:  
+            break;  
+        default:  
+            _ASSERT(false);  
+            break;  
+        }  
+        kind_ = Kind::None;  
+    }  
+  
+    MyVariant(const MyVariant& other)  
+        : kind_(other.kind_)  
+    {  
+        switch (kind_)  
+        {  
+        case Kind::None:  
+            break;  
+        case Kind::A:  
+            new (&a_) A(other.a_);  
+            break;  
+        case Kind::B:  
+            new (&b_) B(other.b_);  
+            break;  
+        case Kind::Integer:  
+            i_ = other.i_;  
+            break;  
+        default:  
+            _ASSERT(false);  
+            break;  
+        }  
+    }  
+  
+    MyVariant(MyVariant&& other)  
+        : kind_(other.kind_)  
+    {  
+        switch (kind_)  
+        {  
+        case Kind::None:  
+            break;  
+        case Kind::A:  
+            new (&a_) A(move(other.a_));  
+            break;  
+        case Kind::B:  
+            new (&b_) B(move(other.b_));  
+            break;  
+        case Kind::Integer:  
+            i_ = other.i_;  
+            break;  
+        default:  
+            _ASSERT(false);  
+            break;  
+        }  
+        other.kind_ = Kind::None;  
+    }  
+  
+    MyVariant& operator=(const MyVariant& other)  
+    {  
+        if (&other != this)  
+        {  
+            switch (other.kind_)  
+            {  
+            case Kind::None:  
+                this->~MyVariant();  
+                break;  
+            case Kind::A:  
+                *this = other.a_;  
+                break;  
+            case Kind::B:  
+                *this = other.b_;  
+                break;  
+            case Kind::Integer:  
+                *this = other.i_;  
+                break;  
+            default:  
+                _ASSERT(false);  
+                break;  
+            }  
+        }  
+        return *this;  
+    }  
+  
+    MyVariant& operator=(MyVariant&& other)  
+    {  
+        _ASSERT(this != &other);  
+        switch (other.kind_)  
+        {  
+        case Kind::None:  
+            this->~MyVariant();  
+            break;  
+        case Kind::A:  
+            *this = move(other.a_);  
+            break;  
+        case Kind::B:  
+            *this = move(other.b_);  
+            break;  
+        case Kind::Integer:  
+            *this = other.i_;  
+            break;  
+        default:  
+            _ASSERT(false);  
+            break;  
+        }  
+        other.kind_ = Kind::None;  
+        return *this;  
+    }  
+  
+    MyVariant(const A& a)  
+        : kind_(Kind::A), a_(a)  
+    {  
+    }  
+  
+    MyVariant(A&& a)  
+        : kind_(Kind::A), a_(move(a))  
+    {  
+    }  
+  
+    MyVariant& operator=(const A& a)  
+    {  
+        if (kind_ != Kind::A)  
+        {  
+            this->~MyVariant();  
+            new (this) MyVariant(a);  
+        }  
+        else  
+        {  
+            a_ = a;  
+        }  
+        return *this;  
+    }  
+  
+    MyVariant& operator=(A&& a)  
+    {  
+        if (kind_ != Kind::A)  
+        {  
+            this->~MyVariant();  
+            new (this) MyVariant(move(a));  
+        }  
+        else  
+        {  
+            a_ = move(a);  
+        }  
+        return *this;  
+    }  
+  
+    MyVariant(const B& b)  
+        : kind_(Kind::B), b_(b)  
+    {  
+    }  
+  
+    MyVariant(B&& b)  
+        : kind_(Kind::B), b_(move(b))  
+    {  
+    }  
+  
+    MyVariant& operator=(const B& b)  
+    {  
+        if (kind_ != Kind::B)  
+        {  
+            this->~MyVariant();  
+            new (this) MyVariant(b);  
+        }  
+        else  
+        {  
+            b_ = b;  
+        }  
+        return *this;  
+    }  
+  
+    MyVariant& operator=(B&& b)  
+    {  
+        if (kind_ != Kind::B)  
+        {  
+            this->~MyVariant();  
+            new (this) MyVariant(move(b));  
+        }  
+        else  
+        {  
+            b_ = move(b);  
+        }  
+        return *this;  
+    }  
+  
+    MyVariant(int i)  
+        : kind_(Kind::Integer), i_(i)  
+    {  
+    }  
+  
+    MyVariant& operator=(int i)  
+    {  
+        if (kind_ != Kind::Integer)  
+        {  
+            this->~MyVariant();  
+            new (this) MyVariant(i);  
+        }  
+        else  
+        {  
+            i_ = i;  
+        }  
+        return *this;  
+    }  
+  
+    Kind GetKind() const  
+    {  
+        return kind_;  
+    }  
+  
+    A& GetA()  
+    {  
+        _ASSERT(kind_ == Kind::A);  
+        return a_;  
+    }  
+  
+    const A& GetA() const  
+    {  
+        _ASSERT(kind_ == Kind::A);  
+        return a_;  
+    }  
+  
+    B& GetB()  
+    {  
+        _ASSERT(kind_ == Kind::B);  
+        return b_;  
+    }  
+  
+    const B& GetB() const  
+    {  
+        _ASSERT(kind_ == Kind::B);  
+        return b_;  
+    }  
+  
+    int& GetInteger()  
+    {  
+        _ASSERT(kind_ == Kind::Integer);  
+        return i_;  
+    }  
+  
+    const int& GetInteger() const  
+    {  
+        _ASSERT(kind_ == Kind::Integer);  
+        return i_;  
+    }  
+  
+private:  
+    Kind kind_;  
+    union  
+    {  
+        A a_;  
+        B b_;  
+        int i_;  
+    };  
+};  
+#pragma warning (pop)  
+  
+int main()  
+{  
+    A a(1, "Hello from A");  
+    B b(2, "Hello from B");  
+  
+    MyVariant mv_1 = a;  
+  
+    cout << "mv_1 = a: " << mv_1.GetA().name << endl;  
+    mv_1 = b;  
+    cout << "mv_1 = b: " << mv_1.GetB().name << endl;  
+    mv_1 = A(3, "hello again from A");  
+    cout << R"aaa(mv_1 = A(3, "hello again from A"): )aaa" << mv_1.GetA().name << endl;  
+    mv_1 = 42;  
+    cout << "mv_1 = 42: " << mv_1.GetInteger() << endl;  
+  
+    b.vec = { 10,20,30,40,50 };  
+  
+    mv_1 = move(b);  
+    cout << "After move, mv_1 = b: vec.size = " << mv_1.GetB().vec.size() << endl;  
+  
+    cout << endl << "Press a letter" << endl;  
+    char c;  
+    cin >> c;  
+}  
+#include <queue>  
+#include <iostream>  
+using namespace std;  
+  
+enum class WeatherDataType  
+{  
+    Temperature, Wind  
+};  
+  
+struct TempData  
+{  
+    TempData() : StationId(""), time(0), current(0), maxTemp(0), minTemp(0) {}  
+    TempData(string id, time_t t, double cur, double max, double min)  
+        : StationId(id), time(t), current(cur), maxTemp(max), minTemp(0) {}  
+    string StationId;  
+    time_t time = 0;  
+    double current;  
+    double maxTemp;  
+    double minTemp;  
+};  
+  
+struct WindData  
+{  
+    int StationId;  
+    time_t time;  
+    int speed;  
+    short direction;  
+};  
+  
+struct Input  
+{  
+    Input() {}  
+    Input(const Input&) {}  
+  
+    ~Input()  
+    {  
+        if (type == WeatherDataType::Temperature)  
+        {  
+            temp.StationId.~string();  
+        }  
+    }  
+  
+    WeatherDataType type;  
+    void SetTemp(const TempData& td)  
+    {  
+        type = WeatherDataType::Temperature;  
+  
+        // must use placement new because of string member!  
+        new(&temp) TempData(td);  
+    }  
+  
+    TempData GetTemp()  
+    {  
+        if (type == WeatherDataType::Temperature)  
+            return temp;  
+        else  
+            throw logic_error("Can't return TempData when Input holds a WindData");  
+    }  
+    void SetWind(WindData wd)  
+    {  
+        // Explicitly delete struct member that has a   
+        // non-trivial constructor  
+        if (type == WeatherDataType::Temperature)  
+        {  
+            temp.StationId.~string();  
+        }  
+        wind = wd; //placement new not required.  
+    }  
+    WindData GetWind()  
+    {  
+        if (type == WeatherDataType::Wind)  
+        {  
+            return wind;  
+        }  
+        else  
+            throw logic_error("Can't return WindData when Input holds a TempData");  
+    }  
+  
+private:  
+  
+    union  
+    {  
+        TempData temp;  
+        WindData wind;  
+    };  
+};  
+  
