40bc596158dfbd37425d4e337add12a7accccedc
Og192 <og192liu@gmail.com>
2017-05-16 10:45:23 +0800

    print \x4dO

---
+++ cppDeepLearn/escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "\n";
+    cout << "\tHi!\n";
+    cout << "Hi \x4dO\115 !\n";
+    cout << '\115' << '\n';
+
+    return 0;
+}
