c8fb9d25da9a4fd0f57a25b63784a6639d459abf
zhuango <Og192liu@163.com>
2017-09-21 21:46:58 +0800

    size of different type

---
+++ cppDeepLearn/basicType/sizeofType.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "bool:\t" << sizeof(bool) << endl;
+    cout << "char:\t" << sizeof(char) << endl;
+    cout << "wchar_t:\t" << sizeof(wchar_t) << endl;
+    cout << "short:\t" << sizeof(short) << endl;
+    cout << "int:\t" << sizeof(int) << endl;
+    cout << "long:\t" << sizeof(long) << endl;
+    cout << "longlong:\t" << sizeof(long long) << endl;
+    cout << "float:\t" << sizeof(float) << endl;
+    cout << "double:\t" << sizeof(double) << endl;
+    cout << "long double:\t" << sizeof(long double) << endl;
+
+    return 0;
+}
