b5d676e45df370f8ebab92a12be7910137b8487e
Og192 <og192liu@gmail.com>
2017-10-26 19:27:42 +0800

    amb

--- cppDeepLearn/functions/overload.cpp
+++ cppDeepLearn/functions/overload.cpp
@@ -7,15 +7,32 @@ void test(int a)
     cout << a << endl;
 }
 
+// top-level const is ignored.
 // same as "void test(int a)" except "a" cannot be modified in this function.
 // Thus this function cannot be a overloaded function of "void test(int a)"
-void test(const int a)
+// void test(const int a)
+// {
+//     // a = a + 1; // error
+//     cout << a << endl;
+// }
+
+int Add(int a, int b)
 {
-    // a = a + 1; // error
-    cout << a << endl;
+    cout << "add two int number" << endl;
+    return a + b;
 }
 
+float Add(float a, float b)
+{
+    cout << "add two float number" << endl;
+    return a + b;
+}
 int main(void)
 {
+    float result = Add(1.6, 2.0);
+    // 3 it is.
+    cout << result << endl;
+
+    float re = Add (1.6, 2.4);
     return 0;
 }
